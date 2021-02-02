436058e39b00010c081edec9954025c082f58fa6
gabriel <vieyra.gabriel08@gmail.com>
2021-02-02 19:25:01 -0300

    validar formulario

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,66 @@ document.addEventListener('DOMContentLoaded', () => {
     const contenedor = document.querySelector('#app');
     contenedor.innerHTML = templateElement.innerHTML;
     const formulario = document.querySelector('form');
-    const inputTitulo = document.querySelector('#title');
-    const inputDescripcion = document.querySelector('#description');
-    const inputGente = document.querySelector('#people');
-    console.log(inputTitulo);
-    console.log(inputDescripcion);
-    console.log(inputGente);
     formulario.setAttribute('id', 'user-input');
+    formulario.addEventListener('submit', enviarFormulario);
 });
+function enviarFormulario(e) {
+    e.preventDefault();
+    const inputTitulo = document.querySelector('#title');
+    const textAreaDescripcion = document.querySelector('#description');
+    const inputGente = document.querySelector('#people');
+    const inputUsuario = validarFormulario(inputTitulo, textAreaDescripcion, inputGente);
+    if (Array.isArray(inputUsuario)) {
+        const [titulo, descripcion, gente] = inputUsuario;
+        console.log(titulo, descripcion, gente);
+    }
+    limpiarInputs(inputTitulo, textAreaDescripcion, inputGente);
+}
+function validarFormulario(inputUno, textArea, inputDos) {
+    const validacionTitulo = {
+        value: inputUno.value,
+        required: true
+    };
+    const validacionDescripcion = {
+        value: textArea.value,
+        required: true,
+        minLenght: 5
+    };
+    const validacionGente = {
+        value: Number(inputDos.value),
+        required: true,
+        min: 1,
+        max: 5
+    };
+    if (!validacion(validacionTitulo) || !validacion(validacionDescripcion) || !validacion(validacionGente)) {
+        alert('Input invalido, por favor intente otra vez!');
+        return;
+    }
+    else {
+        return [inputUno.value, textArea.value, Number(inputDos.value)];
+    }
+}
+function validacion(validarInput) {
+    let esValido = true;
+    if (validarInput.required) {
+        esValido = esValido && validarInput.value.toString().trim().length !== 0;
+    }
+    if (validarInput.minLenght != null && typeof validarInput.value === 'string') {
+        esValido = esValido && validarInput.value.length >= validarInput.minLenght;
+    }
+    if (validarInput.maxLenght != null && typeof validarInput.value === 'string') {
+        esValido = esValido && validarInput.value.length <= validarInput.maxLenght;
+    }
+    if (validarInput.min != null && typeof validarInput.value === 'number') {
+        esValido = esValido && validarInput.value >= validarInput.min;
+    }
+    if (validarInput.max != null && typeof validarInput.value === 'number') {
+        esValido = esValido && validarInput.value <= validarInput.max;
+    }
+    return esValido;
+}
+function limpiarInputs(inputUno, textArea, inputDos) {
+    inputUno.value = '';
+    textArea.value = '';
+    inputDos.value = '';
+}
--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,89 @@ document.addEventListener('DOMContentLoaded', () => {
     contenedor.innerHTML = templateElement.innerHTML;
 
     const formulario = document.querySelector('form') as HTMLFormElement;
-    const inputTitulo = document.querySelector('#title') as HTMLInputElement;
-    const inputDescripcion = document.querySelector('#description') as HTMLTextAreaElement;
-    const inputGente = document.querySelector('#people') as HTMLInputElement;
-    console.log(inputTitulo);
-    console.log(inputDescripcion);
-    console.log(inputGente);
-    
     formulario.setAttribute('id', 'user-input');
+
+    formulario.addEventListener('submit', enviarFormulario);
 })
+
+function enviarFormulario(e: Event) {
+    e.preventDefault();
+
+    const inputTitulo = document.querySelector('#title') as HTMLInputElement;
+    const textAreaDescripcion = document.querySelector('#description') as HTMLTextAreaElement;
+    const inputGente = document.querySelector('#people') as HTMLInputElement;
+    const inputUsuario = validarFormulario(inputTitulo, textAreaDescripcion, inputGente);
+
+    if(Array.isArray(inputUsuario)) {
+        const [titulo, descripcion, gente] = inputUsuario;
+        console.log(titulo, descripcion, gente);
+    }
+
+    limpiarInputs(inputTitulo, textAreaDescripcion, inputGente);
+}
+
+function validarFormulario(inputUno: HTMLInputElement, textArea: HTMLTextAreaElement, inputDos: HTMLInputElement): [string, string, number] | void {
+    const validacionTitulo: Validacion = {
+        value: inputUno.value,
+        required: true
+    };
+
+    const validacionDescripcion: Validacion = {
+        value: textArea.value,
+        required: true,
+        minLenght: 5
+    };
+
+    const validacionGente: Validacion = {
+        value: Number(inputDos.value),
+        required: true,
+        min: 1,
+        max: 5
+    };
+
+    if(!validacion(validacionTitulo) || !validacion(validacionDescripcion) || !validacion(validacionGente)) {
+        alert('Input invalido, por favor intente otra vez!');
+        return;
+    }else {
+        return [inputUno.value, textArea.value, Number(inputDos.value)];
+    }
+}
+
+function validacion(validarInput: Validacion){
+    let esValido = true;
+
+    if(validarInput.required) {
+        esValido = esValido && validarInput.value.toString().trim().length !== 0;
+    }
+    if(validarInput.minLenght != null && typeof validarInput.value === 'string') {
+        esValido = esValido && validarInput.value.length >= validarInput.minLenght;
+    }
+    if(validarInput.maxLenght != null && typeof validarInput.value === 'string') {
+        esValido = esValido && validarInput.value.length <= validarInput.maxLenght;
+    }
+    if(validarInput.min != null && typeof validarInput.value === 'number') {
+        esValido = esValido && validarInput.value >= validarInput.min;
+    }
+    if(validarInput.max != null && typeof validarInput.value === 'number') {
+        esValido = esValido && validarInput.value <= validarInput.max;
+    }
+    return esValido;
+}
+
+function limpiarInputs(inputUno: HTMLInputElement, textArea: HTMLTextAreaElement, inputDos: HTMLInputElement) {
+    inputUno.value = '';
+    textArea.value = '';
+    inputDos.value = '';
+}
+
+interface Validacion {
+    value: string | number;
+    required?: boolean;
+    minLenght?: number;
+    maxLenght?: number;
+    min?: number;
+    max?: number;
+  }
 
 
 
